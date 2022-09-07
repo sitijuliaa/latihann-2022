124c7908f4f1c342043c2578567ea3200fa09620
sitijuliaa <110010453+sitijuliaa@users.noreply.github.com>
2022-09-07 10:55:37 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <form action="session.php" method="post">
+        <div>
+            Username : <input type="text" name="username" id="">
+        </div>
+        <div>
+           Password <input type="password" name="password" id="">
+        </div>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
